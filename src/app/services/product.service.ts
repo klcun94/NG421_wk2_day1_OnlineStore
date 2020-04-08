e4a3c92259e7fae19d5006080119b20776631e3a
klcun94 <klcun94@icloud.com>
2020-04-07 21:30:37 -0500

    add product xervice

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Iproduct } from '../models/iproduct';
+import { products } from '../products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  shoppingCart: Array<Iproduct>;
+  products: Array<Iproduct>;
+  constructor() {
+    this.products = products;
+  }
+
+  getProducts(): Array<Iproduct> {
+    return this.products;
+  }
+
+  getShoppingCart(): Array<Iproduct> {
+    return this.shoppingCart;
+  }
+
+  addToCart(product: Iproduct) {
+    this.shoppingCart.push(product);
+  }
+}
