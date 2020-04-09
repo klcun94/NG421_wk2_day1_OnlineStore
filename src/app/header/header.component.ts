2fafb9dc109fb1073863913890b66572f17313ab
klcun94 <klcun94@icloud.com>
2020-04-09 15:37:15 -0500

    added shopping cart count

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Iproduct } from '../models/iproduct';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  shoppingCart: Iproduct[];
 
-  ngOnInit() {
+  constructor(private productService: ProductService) {
   }
 
+  ngOnInit() {
+    this.shoppingCart = this.productService.getShoppingCart();
+  }
 }
