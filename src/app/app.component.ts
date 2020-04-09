0e0c83ced50ef14aae6d4aa72b877903ee741314
klcun94 <klcun94@icloud.com>
2020-04-08 20:05:56 -0500

    get products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Iproduct } from './models/iproduct';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  products: Array<Iproduct>;
+
+  constructor(private productService: ProductService) {
+
+  }
+  ngOnInit(){
+    this.products = this.productService.getProducts();
+  }
 }
